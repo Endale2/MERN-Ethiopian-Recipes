3c916d46bed6f1a3a86d5d5373e45acaa51ca58b
Endale2 <endale406@gmail.com>
2024-08-01 06:45:10 +0300

    small ui modification

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import "../App.css"
 import {useCookies } from"react-cookie"
 import { useNavigate} from "react-router-dom"
+import "./styles/NavBar.css"
 
 function NavBar() {
   const [cookies, setCookies] = useCookies(["access_tokens"])
@@ -16,9 +17,9 @@ function NavBar() {
 
   return (
     <div className="navbar">
-      <Link to="/">Home</Link>
-      <Link to="/create-recipes">Create Recipes</Link>
-      <Link to="/saved-recipes">Saved Recipes</Link>
+      <Link className="home-link" to="/">Home</Link>
+      <Link className="create-recipes" to="/create-recipes">Create Recipes</Link>
+      <Link className="saved-recipes" to="/saved-recipes">Saved Recipes</Link>
       { !cookies.access_tokens?(
         <>
       <Link to="/Login">Login</Link>
