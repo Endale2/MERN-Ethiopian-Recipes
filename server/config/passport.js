fada059674b6a66680a74e8dafc39b8137f78a2b
Endale2 <endale406@gmail.com>
2025-06-24 12:34:03 +0300

    updated

--- server/config/passport.js
+++ server/config/passport.js
@@ -15,7 +15,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL:  `${process.env.BACKEND_URL}/auth/google/callback`,
+      callbackURL:  `https://mern-ethiopian-recipes.onrender.com/auth/google/callback`,
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
